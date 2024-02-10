1c48e46bc7438ceefd45778a372372075663eaa8
pawan-taak <pawantaak786@gamil.com>
2024-02-10 18:38:26 +0530

    header update

--- src/componets/header.jsx
+++ src/componets/header.jsx
@@ -7,10 +7,8 @@ function Header() {
             {/* top header section */}
 
             <div className="container">
-                <div className="section">
-                    <div className="header_top_section">
-                        <p className="h_mm_heading">Sign up and get 20% off to your first order. <span>Sign Up Now</span></p>
-                    </div>
+                <div className="header_top_section">
+                    <p className="h_mm_heading">Sign up and get 20% off to your first order. <span className="sign_up">Sign Up Now</span></p>
                 </div>
             </div>
 
@@ -18,7 +16,11 @@ function Header() {
 
             <div className="section">
                 <div className="header_menu_section">
-                    <h3 className="h_heading">SHOP.CO</h3>
+                    <div className="toggle_bar">
+                        <img src={require('./header/assest/icon/toggle.png')} className="toggle_button" />
+                        <h3 className="h_heading">SHOP.CO</h3>
+                    </div>
+
                     <ul className="header_menu_item  h_sm_heading">
                         <li>Shop</li>
                         <li>On Sale</li>
@@ -26,12 +28,13 @@ function Header() {
                         <li>Brands</li>
                     </ul>
                     <div className="header_input_section">
-                        <p>Q</p>
+                        <img src={require('./header/assest/icon/header_search_icon.png')} />
                         <input type="text" placeholder="Search for products..." className="h_sm_heading" />
                     </div>
                     <div className="header_icon">
-                        <p>Q</p>
-                        <p>Q</p>
+                        <img src={require('./header/assest/icon/header_search_icon.png')} className="menu_search_icon" />
+                        <img src={require('./header/assest/icon/header_cart_icon.png')} />
+                        <img src={require('./header/assest/icon/header_user_icon.png')} />
                     </div>
                 </div>
             </div>
@@ -43,11 +46,12 @@ function Header() {
                     <div className="section">
                         <div className="hero_section">
                             <div className="hero_text_section">
-                                <h1 className="h_primary">FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
-                                <p className="h_sm_heading">Browse through our diverse range of meticulously crafted garments,
+                                <h1 className="hero_main_heading h_primary">FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
+                                <p className="hero_main_text h_sm_heading">Browse through our diverse range of meticulously crafted garments,
                                     designed to bring out your individuality and cater to your sense of style.
                                 </p>
                                 <button className="hero_button h_sm_prime">Shop Now</button>
+
                                 <div className="hero_sub_section">
                                     <div>
                                         <h2 className="h_secondary">200+</h2>
@@ -55,22 +59,39 @@ function Header() {
                                     </div>
                                     <div class="vl"></div>
                                     <div>
-                                        <h2 className="h_secondary">200+</h2>
-                                        <p className="h_sm_heading">International Brands</p>
+                                        <h2 className="h_secondary">2,000+</h2>
+                                        <p className="h_sm_heading">High-Quality Products</p>
                                     </div>
                                     <div class="vl"></div>
-                                    <div>
-                                        <h2 className="h_secondary">200+</h2>
-                                        <p className="h_sm_heading">International Brands</p>
+                                    <div className="price_text">
+                                        <h2 className="h_secondary">30,000+</h2>
+                                        <p className="h_sm_heading">Happy Customers</p>
                                     </div>
                                 </div>
                             </div>
-                            <div>img section</div>
+                            <div className="hero_img">
+                                <img src={require('./header/assest/img/hero_img.png')} />
+                            </div>
                         </div>
 
                     </div>
                 </div>
             </div>
+
+            {/* company name section */}
+
+            <div className="company_back_section">
+                <div className="section">
+                    <div className="company_name_section">
+                        <img src={require('./header/assest/img/versace.png')} className="company_img" />
+                        <img src={require('./header/assest/img/zara-logo.png')} className="company_img" />
+                        <img src={require('./header/assest/img/gucci-logo.png')} className="company_img" />
+                        <img src={require('./header/assest/img/prada-logo.png')} className="company_img" />
+                        <img src={require('./header/assest/img/calvin.png')} className="company_img" />
+                    </div>
+                </div>
+            </div>
+
 
 
 
