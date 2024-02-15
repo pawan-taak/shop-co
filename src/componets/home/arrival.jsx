34850517edbcf267abab7fbca4be8d8707f60e99
suryakhichar <jjatt7213@gmail.com>
2024-02-15 21:18:45 +0530

    arrivals pics

--- src/componets/home/arrival.jsx
+++ src/componets/home/arrival.jsx
@@ -3,22 +3,25 @@ import Card from "../../snippets/card";
 
 const Arrival=()=>{
   const data = [
-    {
+    { img:'tshirt_tape.png',
       title: 'T-SHIRT WITH TAPE DETAILS',
       stars: 5,
       price: '$120',
     },
     {
+        img:'skinny_fitjeans.png',
       title: 'SKINNY FIT JEANS',
       stars: 5,
       price: '$240',       
     },
     {
+        img:'cheaked_shirt.png',
       title: 'CHECKERED SHIRT',
       stars: 5,
       price: '$180',      
     },
     {
+        img:'sleeve_shirt.png',
       title: 'SLEEVE STRIPED T-SHIRT',
       stars: 5,
       price: '$130',      
@@ -29,6 +32,8 @@ const Arrival=()=>{
       return(
         // <h2>{item?.title}</h2>
         <>
+       
+        <img src={require(`../../assets/img/arrivals_img/${item?.img}`)}></img>
         <h2>{item?.title}</h2>
         <img src={require('../../assets/img/star.png')}></img>
         <p>{item?.price}</p>
