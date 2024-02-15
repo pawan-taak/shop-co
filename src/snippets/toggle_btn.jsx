7c6fb67a657213a74e784cf40d79579a4be0d742
pawan-taak <pawantaak786@gamil.com>
2024-02-15 13:36:00 +0530

    toggle update

--- src/snippets/toggle_btn.jsx
+++ src/snippets/toggle_btn.jsx
@@ -1,23 +1,13 @@
 import React, { useState } from "react"
 import "../assets/css/toggle_btn.css"
 
-function ToggleBtn() {
-    let [state, setShow] = useState(false)
+function ToggleBtn({show}) {
 
     return (
 
-        <div>
-            <button onClick={() => { setShow(!state) }} class="toggle_button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <path d="M3 12H21" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M3 6H21" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M3 18H21" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-            </button>
-
-        
+        <div className={`${show===true?"active":""} sidebar`} >       
          {
-                state &&
+                // state &&
                 <ul className="menu_bar h_sm_heading">
                     <li>Shop</li>
                     <li>On Sale</li>
