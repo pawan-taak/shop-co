5314dd92fa5337914c1794affb29cc53d4a735b8
suryakhichar <jjatt7213@gmail.com>
2024-02-15 21:40:44 +0530

    update

--- src/componets/home/arrival.jsx
+++ src/componets/home/arrival.jsx
@@ -1,49 +1,58 @@
 import React, { Fragment } from "react";
 import Card from "../../snippets/card";
+import '../../assets/css/arrivals.css'
 
-const Arrival=()=>{
-  const data = [
-    { img:'tshirt_tape.png',
-      title: 'T-SHIRT WITH TAPE DETAILS',
-      stars: 5,
-      price: '$120',
-    },
-    {
-        img:'skinny_fitjeans.png',
-      title: 'SKINNY FIT JEANS',
-      stars: 5,
-      price: '$240',       
-    },
-    {
-        img:'cheaked_shirt.png',
-      title: 'CHECKERED SHIRT',
-      stars: 5,
-      price: '$180',      
-    },
-    {
-        img:'sleeve_shirt.png',
-      title: 'SLEEVE STRIPED T-SHIRT',
-      stars: 5,
-      price: '$130',      
-    },
-  ];
-  return(
-    data.map((item,index)=>{
-      return(
-        // <h2>{item?.title}</h2>
-        <>
-       
-        <img src={require(`../../assets/img/arrivals_img/${item?.img}`)}></img>
-        <h2>{item?.title}</h2>
-        <img src={require('../../assets/img/star.png')}></img>
-        <p>{item?.price}</p>
+const Arrival = () => {
+    const data = [
+        {
+            img: 'tshirt_tape.png',
+            title: 'T-SHIRT WITH TAPE DETAILS',
+            stars: 5,
+            price: '$120',
+        },
+        {
+            img: 'skinny_fitjeans.png',
+            title: 'SKINNY FIT JEANS',
+            stars: 5,
+            price: '$240',
+        },
+        {
+            img: 'cheaked_shirt.png',
+            title: 'CHECKERED SHIRT',
+            stars: 5,
+            price: '$180',
+        },
+        {
+            img: 'sleeve_shirt.png',
+            title: 'SLEEVE STRIPED T-SHIRT',
+            stars: 5,
+            price: '$130',
+        },
+    ];
+    return (
+        <div className="section">
+        <h2>NEW ARRIVALS</h2>
+            <div className="shirts_scetion">
+                {
+                    data.map((item, index) => {
+                        return (
+                            // <h2>{item?.title}</h2>
+                            <>
+                                <div>
+                                    <img src={require(`../../assets/img/arrivals_img/${item?.img}`)}></img>
+                                    <h2>{item?.title}</h2>
+                                    <img src={require('../../assets/img/star.png')}></img>
+                                    <p>{item?.price}</p>
+                                </div>
 
+                            </>
+                        )
+                    })
 
-        </>
-      ) 
-    })
-
-  )
+                }
+            </div>
+        </div>
+    )
 
 }
 
