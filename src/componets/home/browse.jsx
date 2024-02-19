e45570ba232fe5179a85d1c901bcafd2aae32ffc
shishpal-nokhwal <nokhwalshishpal29@gmail.com>
2024-02-19 13:50:21 +0530

    mob update

--- src/componets/home/browse.jsx
+++ src/componets/home/browse.jsx
@@ -2,38 +2,64 @@ import React from 'react'
 import '../../assets/css/browse.css'
 
 const Browse = () => {
-  return (
-    <div className="container">
-                <div className="section">
-                    <div className="browse_section">
-                        
-                        <h2 className="m_primary_heading">BROWSE BY DRESS STYLE</h2>
+    return (
+        <div className="container">
+            <div className="section">
+                <div className="browse_section">
 
-                        <div className="browse_card_section">
-                            <span>
-                                <p className="browse_heading m_secondary_heading">Casual</p>
-                                <img src={require('../../assets/img/casual_img.png')} alt="browse_img causal_img" />
-                            </span>
+                    <h2 className="m_primary_heading">BROWSE BY DRESS STYLE</h2>
+                    <div className="browse_card_section">
+                        <span>
+                            <p className="browse_heading m_secondary_heading">Casual</p>
+                            <img src={require('../../assets/img/casual_img.png')} alt="browse_img causal_img" />
+                        </span>
 
-                            <span>
-                                <p className="browse_heading m_secondary_heading">Formal</p>
-                                <img src={require('../../assets/img/formal_img.png')} alt='#' className="browse_img formal_img"/>
-                            </span>
+                        <span>
+                            <p className="browse_heading m_secondary_heading">Formal</p>
+                            <img src={require('../../assets/img/formal_img.png')} alt='#' className="browse_img formal_img" />
+                        </span>
 
-                            <span>
-                                <p className="browse_heading m_secondary_heading">Party</p>
-                                <img src={require('../../assets/img/party_img.png')}alt='#' className="browse_img party_img" />
-                            </span>
+                        <span>
+                            <p className="browse_heading m_secondary_heading">Party</p>
+                            <img src={require('../../assets/img/party_img.png')} alt='#' className="browse_img party_img" />
+                        </span>
 
-                            <span>
-                                <p className="browse_heading m_secondary_heading">Gym</p>
-                                <img src={require('../../assets/img/gym_img.png')} alt='#' className="browse_img gym_img" />
-                            </span>
-                        </div>
+                        <span>
+                            <p className="browse_heading m_secondary_heading">Gym</p>
+                            <img src={require('../../assets/img/gym_img.png')} alt='#' className="browse_img gym_img" />
+                        </span>
+                    </div>
+
+
+                    {/* mob section */}
+
+
+                    <div className="browse_mob_section">
+                        <span>
+                            <p className="browse_mob_heading m_secondary_heading">Casual</p>
+                            <img src={require('../../assets/img/casual_mb_img.png')} alt="browse_mob_img causal_img" />
+                        </span>
+
+                        <span>
+                            <p className="browse_mob_heading m_secondary_heading">Formal</p>
+                            <img src={require('../../assets/img/formal_mb_img.png')} alt='#' className="browse_mob_img formal_img" />
+                        </span>
+
+                        <span>
+                            <p className="browse_mob_heading m_secondary_heading">Party</p>
+                            <img src={require('../../assets/img/party_mb_img.png')} alt='#' className="browse_mob_img party_img" />
+                        </span>
+
+                        <span>
+                            <p className="browse_mob_heading m_secondary_heading">Gym</p>
+                            <img src={require('../../assets/img/gym_mb_img.png')} alt='#' className="browse_mob_img gym_img" />
+                        </span>
                     </div>
                 </div>
+
             </div>
-  )
+        </div>
+    )
 }
 
 export default Browse
