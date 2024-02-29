489ef95e58af201a16bf55cc953c3f5e70ff880c
pawan-taak <pawantaak786@gmail.com>
2024-02-29 12:47:48 +0530

    update images path

--- src/snippets/card.jsx
+++ src/snippets/card.jsx
@@ -1,26 +1,25 @@
 import { useEffect, useState } from 'react'
 import '../assets/css/card.css'
 
-const Card=({data, filters})=>{
-    let [filteredData, setFilteredData]= useState(data)
-    useEffect=(()=>{
-       
-        filterData()
-    },[])
+const Card = ({ data, filters }) => {
+    let [filteredData, setFilteredData] = useState(data)
+    useEffect = (() => {
 
-const filterData=()=>{
- if(filteredData.title===filters)
-        {
+        filterData()
+    }, [])
+
+    const filterData = () => {
+        if (filteredData.title === filters) {
             setFilteredData(filteredData)
         }
-}
+    }
 
-    return(
+    return (
         <div className='common_card_section'>
-        <img src={require(`../assets/img/${data.img}`)} alt='#'/>
-        <h2 className='card_title m_md_heading'>{data?.title}</h2>
-        <img src={require('../assets/img/star.png')} alt='#' />
-        <p className='m_lg_heading'>{data?.price}</p>
+            <img src={require(`../assets/img/${data.img}`)} alt='#' />
+            <h2 className='card_title m_md_heading'>{data?.title}</h2>
+            <img src={require('../assets/img/star.png')} alt='#' />
+            <p className='m_lg_heading'>${data?.price}</p>
         </div>
     )
 }
